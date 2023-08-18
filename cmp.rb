aba7fdd456eec1448ccd088bcaa5576f3ee4c585
william-vw <wvw@Williams-MacBook-Pro.local>
2023-08-18 17:36:41 -0400

    uncommented 'diff' code

--- cmp.rb
+++ cmp.rb
@@ -19,22 +19,22 @@ puts "|graphc2| = " + graphc2.count().to_s
 File.open("canon/" + file1, "w") { |f| f.write(graphc1.dump(:ntriples)) }
 File.open("canon/" + file2, "w") { |f| f.write(graphc2.dump(:ntriples)) }
 
-# puts "\nmissing in graph2:"
-# miss2_cnt = 0
-# graph1.each_statement do |stmt|
-#     if !graph2.statement?(stmt)
-#         miss2_cnt += 1
-#         puts stmt
-#     end
-# end
-# puts "# missing in graph2: " + miss2_cnt.to_s
+puts "\nmissing in graph2:"
+miss2_cnt = 0
+graph1.each_statement do |stmt|
+    if !graph2.statement?(stmt)
+        miss2_cnt += 1
+        puts stmt
+    end
+end
+puts "# missing in graph2: " + miss2_cnt.to_s
 
-# puts "\nmissing in graph1:"
-# miss1_cnt = 0
-# graph2.each_statement do |stmt|
-#     if !graph1.statement?(stmt)
-#         miss1_cnt += 1
-#         puts stmt
-#     end
-# end
-# puts "# missing in graph1: " + miss1_cnt.to_s
+puts "\nmissing in graph1:"
+miss1_cnt = 0
+graph2.each_statement do |stmt|
+    if !graph1.statement?(stmt)
+        miss1_cnt += 1
+        puts stmt
+    end
+end
+puts "# missing in graph1: " + miss1_cnt.to_s
